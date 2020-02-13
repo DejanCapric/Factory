c8acebe7be76a5dee5a173094b824d7681310efd
Dejan <capricdejan@gmail.com>
2020-02-13 20:18:37 +0100

    passenger

--- src/main/java/main/factory/Car.java
+++ src/main/java/main/factory/Car.java
@@ -1,22 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main.factory;
-
-
- 
 public class Car {
     private final String vehicleType = "car";
     private String model;
-    private  int buildYear;
+    private int buildYear;
     private int maxSpeed;
     private String color;
-    private  int maxFuel;
-    private  int currentFuel;
-    private  int consumption;
-    
+    private int maxFuel;
+    private int currentFuel;
+    private int consumption;
+    private double mileage;
     public Car() {
         this.model = "";
         this.buildYear = 0;
@@ -24,44 +16,46 @@ public class Car {
         this.maxFuel = 0;
         this.currentFuel = 0;
         this.consumption = 0;
+        this.mileage = 0;
     }
-    public Car(String customModel, int customBuildYear, String customColor, int customMaxFuel, int customCurrentFuel, int customconsuption) {
+    public  Car(String customModel, int customBuildYear, int customMaxSpeed, String customColor, int customMaxFuel, int customCurrentFuel, int customconsumption, double customMileage) {
         this.model = customModel;
         this.buildYear = customBuildYear;
+        this.maxSpeed = customMaxSpeed;
         this.color = customColor;
         this.maxFuel = customMaxFuel;
-        this.currentFuel = customCurrentFuel; 
-        this.consumption = customconsuption;
-        
+        this.currentFuel = customCurrentFuel;
+        this.consumption = customconsumption;
+        this.mileage = customMileage;
     }
-    
-    
+    public String getVehicleType() {
+        return this.vehicleType;
+    }
     public String getModel() {
         return this.model;
     }
-    
     public void setModel(String customModel) {
         this.model = customModel;
     }
-    public int getBuildYear(){
+    public int getBuildYear() {
         return this.buildYear;
     }
     public void setBuildYear(int customBuildYear) {
-        this.buildYear = customBuildYear;
+                this.buildYear = customBuildYear;
     }
-    public String getColor() {
-    return this.color;
-    }
-    public void setColor(String customColor){
-        this.color = customColor;
-    }
-     public int getMaxSpeed() {
+    public int getMaxSpeed() {
         return this.maxSpeed;
     }
     public void setMaxSpeed(int customMaxSpeed) {
                 this.maxSpeed = customMaxSpeed;
     }
-     public int getMaxFuel() {
+    public String getColor() {
+        return this.color;
+    }
+    public void setColor(String customColor) {
+        this.color = customColor;
+    }
+    public int getMaxFuel() {
         return this.maxFuel;
     }
     public void setMaxFuel(int customMaxFuel) {
@@ -76,28 +70,51 @@ public class Car {
     public int getConsumption() {
         return this.consumption;
     }
-    public void setConsumption(int customconsumption) {
+    public void setconsumption(int customconsumption) {
                 this.consumption = customconsumption;
     }
-    public String getVehicleType(){
-        return this.vehicleType;
+    public double getMileage() {
+        return this.mileage;
     }
-    
+    public void setMileage(double customMileage) {
+                this.mileage = customMileage;
+    }
     public void showData() {
-        System.out.println("Model: " + this.model);
-        System.out.println("Godina proizvodnje: " + this.buildYear);
-        System.out.println("Boja: " + this.color);
-        System.out.println("Potrosnja: " + this.getConsumption());
-        System.out.println("Stanje reyervoara: " + this.getCurrentFuel());
+        System.out.println("Model: " + this.getModel());
+        System.out.println("Godina proizvodnje: " + this.getBuildYear());
+        System.out.println("Boja: " + this.getColor());
+        System.out.println("Potrošnja: " + this.getConsumption());
+        System.out.println("Stanje rezervoara: " + this.getCurrentFuel());
         System.out.println("Kapacitet rezervoara je: " + this.getMaxFuel());
-        System.out.println();
+        System.out.println("Maksimalna brzina: " + this.getMaxSpeed());
+        System.out.println("Kilometraza: " + this.getMileage());
     }
-    public void travel(int distanceTraveled) {
-        //1.definisati novi atribut mileage
-        //2.napraviti get i set metode
-        //3. prosiriti jedan od konstruktora da moye da se setuje i mileage
-        //4. kolicina goriva da se smanji za onoliko koliko je potroseno
-        //
+    public void travel(int distance) {
+        if(this.getCurrentFuel() > (distance * this.getConsumption())/100){
+        this.mileage = this.getMileage() +distance; 
+        this.currentFuel = this.getCurrentFuel() - (distance * this.getConsumption())/100;
+        System.out.println("Uspesno ste putovali: " + distance + "kilometara);
+    }else{
+            System.out.println("Nema dovoljno goriva ya put od: " + distance);
+        }
+    public void fuelUp(int refill) {
+        int emptySpace this.getMaxFuel() - this.getCurrentFuel();
+         
+        if(refill < emptySpace) {
+            this.currentFuel = this.getCurrentFuel() + refill;
+            System.out.println("Uspesno ste stigli" + refill + "Novo stanje je: " + this.getCurrentFuel());
+        }
+        
+    
+        this.currentFuel = this.getCurrentFuel() + refill;
+        
+        }   
+        
+      
     }
-     
-}
+
+
+
+
+
+
--- src/main/java/main/factory/Main.java
+++ src/main/java/main/factory/Main.java
@@ -1,43 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main.factory;
-
 /**
  *
  * @author qa
  */
 public class Main {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         System.out.println("Pocetak");
-         
-         String message = "Ovo je pocetak";
-         Car fiat = new Car();
-//fiat.model = "Fiat Punto";
-         fiat.setModel("Fiat Punto");
-         fiat.setBuildYear (2010);
-         fiat.setColor ("black");
-         fiat.setConsumption(6);
-         fiat.setMaxSpeed (180);
-         fiat.setCurrentFuel(20);
-         fiat.setMaxFuel(45);
-         
-         fiat.showData();
-         
-         System.out.println("--------------------");
-         
-         Car ford = new Car();
- //        ford.model = "Ford Fiesta";
-         ford.setModel("Ford Fiesta");
-         ford.setBuildYear(2015);
-         
-          ford.showData();
-//          System.out.println("Model: " + ford.getModel());
+        System.out.println("Pocetak");
+        String message = "Ovo je poruka";
+        Car fiat = new Car("Fiat Punto", 2010, 180, "Black", 45, 20, 6, 0);
+        fiat.setModel("Fiat Punto");
+        fiat.setBuildYear(2010);
+        fiat.setColor("Black");
+        fiat.setconsumption(6);
+        fiat.setCurrentFuel(20);
+        fiat.setMaxSpeed(180);
+        fiat.setMaxFuel(45);
+        fiat.setMileage(0);
+//        fiat.buildYear = 2010;
+//        fiat.color = "Black";
+//        fiat.consumption = 6;
+//        fiat.maxSpeed = 180;
+//        fiat.currentFuel = 20;
+//        fiat.maxFuel = 45;
+//
+        fiat.showData();
+        System.out.println("________________");
+        Car ford = new Car("Ford", 2015, 190, "Red", 40, 10, 5, 30000);
+//        Car ford = new Car();
+//        ford.setModel("Ford Fiesta");
+//        ford.setBuildYear(2015);
+//        ford.buildYear = 2015;
+        ford.showData();
+        System.out.println("________________");
+//        System.out.println("Model: " + ford.getModel());
+        Car peugeot = new Car("306", 2001, 180, "Red", 40, 7, 6, 15000);
+        peugeot.showData();
+        System.out.println("________________");
+        Car Lada = new Car("Lada Niva", 1980, 190, "green", 40, 20, 10, 12000);
+        Lada.showData();
     }
 }
